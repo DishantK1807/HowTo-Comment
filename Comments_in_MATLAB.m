e478ed17ce205f02a810a3365fcca1d3c3b9dd32
Dishant Khanna <dishant.khanna1807@gmail.com>
2016-03-19 16:35:50 +0530

    Update Comments_in_MATLAB.m

--- Comments_in_MATLAB.m
+++ Comments_in_MATLAB.m
@@ -6,4 +6,22 @@ close all;
 % This is how you add comments in MATLAB.
 % Include a '%' symbol in each line to make it a comment
 
+a=10; % can be added after any command
+
+%{
+this is a
+Block 
+comment
+%}
+a=5;
+%{
+comment
+%{
+This is an 
+example of 
+extened comments
+%}
+extended comments closed
+%}
+
 
